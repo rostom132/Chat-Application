803a87d56a67a82a51da7f91f34add0c01112b19
khoathecoolest <47716484+khoathecoolest@users.noreply.github.com>
2020-05-16 15:05:18 +0700

    New code for sending universal files

--- client.java
+++ client.java
@@ -1,42 +1,28 @@
-import java.net.*;
 import java.io.*;
-import java.util.*;
+import java.net.*;
+
 public class client{
-  public static void main(String[] args) throws IOException{
-      BufferedWriter os = null;
-      BufferedReader is = null;
-      Socket ss = new Socket("27.74.245.186", 8080);
-      System.out.println("hello");
-      clientInfo client = new clientInfo();
-      client.getNickName();
-      System.out.println(client.NickName+" Connected");
-      try{
-        os = new BufferedWriter(new OutputStreamWriter(ss.getOutputStream()));
-        is = new BufferedReader(new InputStreamReader(ss.getInputStream()));
-      } catch(IOException e){
-        System.err.println("Couldn't get I/O for the connection to " + "27.74.245.186");
-        return;
+  public static void main(String[] argv) throws Exception{
+    int fileSize;
+    Socket sr = new Socket("localhost", 8080);
+    DataInputStream dis = new DataInputStream(sr.getInputStream());
+    DataOutputStream dos = new DataOutputStream(sr.getOutputStream());
+    fileSize = Integer.parseInt(dis.readUTF());
+    if(fileSize > 0){
+      dos.writeUTF("OK");
+      byte[] b = new byte[fileSize];
+      InputStream is = sr.getInputStream();
+      File file = new File("./nhan/test.doc");
+      FileOutputStream fos = new FileOutputStream(file);
+      BufferedOutputStream bos = new BufferedOutputStream(fos);
+      int a;
+      while ((a = is.read()) != -1) {
+        bos.write(a);
       }
-      try{
-        System.out.println("Sending message!");
-        os.write("HELO! this is " + client.NickName);
-        os.newLine(); // kết thúc dòng
-        os.flush();  // đẩy dữ liệu đi.
-        os.write("quit");
-        os.newLine();
-        os.flush();
-        String responseLine;
-           while ((responseLine = is.readLine()) != null) {
-               System.out.println("Server: " + responseLine);
-               if (responseLine.indexOf("OK") != -1) {
-                   break;
-               }
-           }
-        os.close();
-        is.close();
-        ss.close();
-      }catch (IOException e) {
-        System.err.println("IOException:  " + e);
-       }
+      bos.flush();
+      bos.close();
+      fos.close();
+      System.out.println("Done!");
+    }
   }
 }
--- server.java
+++ server.java
@@ -1,46 +1,25 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
 public class server{
-  public static void main(String[] args) throws IOException{
-    clientInfo client[] = new clientInfo[10];
-    int clientNum = 0;
-    System.out.println("Open connection on port 8080");
-    ServerSocket ss = new ServerSocket(8080);
-    while(true){
-      Socket s = ss.accept();
-      new ServiceThread(s, clientNum++).start();
-      System.out.println("client connected");
-    }
-  }
-  private static class ServiceThread extends Thread {
-    private int clientNum;
-    private Socket s;
-    public ServiceThread(Socket s, int clientNum) {
-      this.clientNum = clientNum;
-      this.s = s;
-    }
-    @Override
-    public void run(){
-      try{
-        BufferedReader is = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        BufferedWriter os = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-        while(true){
-          String line = is.readLine();
-          os.write(">>"+line);
-          os.newLine();
-          os.flush();
-          if (line.equals("QUIT")) {
-            os.write(">> OK");
-            os.newLine();
-            os.flush();
-            break;
-          }
-        }
-      } catch(IOException e){
-        System.out.println(e);
-        e.printStackTrace();
-      }
+  public static void main(String[] argv) throws Exception{
+    ServerSocket s = new ServerSocket(8080);
+    Socket sr = s.accept();
+    DataInputStream dis = new DataInputStream(sr.getInputStream());
+    DataOutputStream dos = new DataOutputStream(sr.getOutputStream());
+    File file = new File("code.zip");
+    if (!file.exists() || !file.isFile()) return;
+    int fileSize;
+    fileSize = (int)file.length();
+    byte b[] = new byte[fileSize];
+    FileInputStream fis = new FileInputStream(file);
+    BufferedInputStream bis = new BufferedInputStream(fis);
+    dos.writeUTF(Integer.toString(fileSize));
+    if(dis.readUTF().equals("OK")){
+      bis.read(b, 0, b.length);
+      OutputStream os = sr.getOutputStream();
+      os.write(b, 0, b.length);
+      os.flush();
     }
   }
 }
