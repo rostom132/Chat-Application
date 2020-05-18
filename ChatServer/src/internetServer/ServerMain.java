833e6150f7b86a558437e5920cbdf0a75801964f
NhanNgocThien <42461717+NhanNgocThien@users.noreply.github.com>
2020-05-18 08:54:36 +0700

    Delete ServerMain.java

--- ChatServer/src/internetServer/ServerMain.java
+++
@@ -1,9 +0,0 @@
-package internetServer;
-
-public class ServerMain {
-    public static void main(String[] args) {
-        int port = 8818;
-        Server server = new Server(port);
-        server.start();
-    }
-}
